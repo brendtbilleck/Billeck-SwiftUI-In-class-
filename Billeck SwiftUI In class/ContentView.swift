bf5ab7ba8ffa5a5fc8fa8cf1905196dc28169add
Brendt Billeck <brendtbilleck.com>
2020-01-28 12:01:38 -0500

    Initial Commit

---
+++ Billeck SwiftUI In class/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Billeck SwiftUI In class
+//
+//  Created by Brendt Billeck on 1/28/20.
+//  Copyright © 2020 Brendt Billeck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
